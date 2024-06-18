65b72c539728187ef369c9787db25a182fa0869d
922872 <2022675594@student.uitm.edu.my>
2024-06-18 22:17:32 +0800

    a

--- src/main/java/com/heroku/java/controller/PackageController.java
+++ src/main/java/com/heroku/java/controller/PackageController.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -86,10 +87,9 @@ public class PackageController {
 
         return "redirect:/CreatePack"; 
     }
-}
 
 
-    
+
     @PostMapping("/UpdatePack")
     public String updatePack(@ModelAttribute("package") PackageModel packages) {
         try {
