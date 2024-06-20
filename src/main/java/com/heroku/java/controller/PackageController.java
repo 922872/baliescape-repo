698e96f9cd7d845813d2d40386fbda6c818fc07b
922872 <2022675594@student.uitm.edu.my>
2024-06-20 20:31:08 +0800

    a

--- src/main/java/com/heroku/java/controller/PackageController.java
+++ src/main/java/com/heroku/java/controller/PackageController.java
@@ -1,11 +1,4 @@
 package com.heroku.java.controller;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.heroku.java.model.PackageModel;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PackageController {
@@ -28,113 +29,80 @@ public class PackageController {
 
     @PostMapping("/CreatePack")
     public String createPack(@ModelAttribute("package") PackageModel packages) {
-        try {
-            Connection connection = dataSource.getConnection();
-            String sql = "INSERT INTO public.package(packid, packname, packactivity, packprice) VALUES(?,?,?,?)";
-            final var statement = connection.prepareStatement(sql);
-    
-            Integer packID = packages.getPackID();
-            String packName = packages.getPackName();
-            String packActivity = packages.getPackActivity();
-            double packPrice = packages.getPackPrice();
-    
-            statement.setInt(1, packID);
-            statement.setString(2, packName);
-            statement.setString(3, packActivity);
-            statement.setDouble(4, packPrice);
+        String sql = "INSERT INTO public.package(packid, packname, packactivity, packprice) VALUES(?,?,?,?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-    
+            statement.setInt(1, packages.getPackID());
+            statement.setString(2, packages.getPackName());
+            statement.setString(3, packages.getPackActivity());
+            statement.setDouble(4, packages.getPackPrice());
+
             statement.executeUpdate();
-            connection.close();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return "redirect:/error";
         }
-    
         return "redirect:/CreatePack";
     }
-     
+
     @GetMapping("/packageList")
     public String packageList(Model model) {
         List<PackageModel> packages = new ArrayList<>();
+        String sql = "SELECT packid, packname, packactivity, packprice FROM public.package ORDER BY packid";
 
-        try (Connection connection = dataSource.getConnection()) {
-            String sql = "SELECT packid, packname, packactivity, packprice FROM public.package ORDER BY packid";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet resultSet = statement.executeQuery();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
 
             while (resultSet.next()) {
-                Integer packID = resultSet.getInt("packid");
-                String packName = resultSet.getString("packname");
-                String packActivity = resultSet.getString("packactivity");
-                double packPrice = resultSet.getDouble("packprice");
-
                 PackageModel packageModel = new PackageModel();
-                packageModel.setPackID(packID);
-                packageModel.setPackName(packName);
-                packageModel.setPackActivity(packActivity);
-                packageModel.setPackPrice(packPrice);
-
+                packageModel.setPackID(resultSet.getInt("packid"));
+                packageModel.setPackName(resultSet.getString("packname"));
+                packageModel.setPackActivity(resultSet.getString("packactivity"));
+                packageModel.setPackPrice(resultSet.getDouble("packprice"));
                 packages.add(packageModel);
             }
 
             model.addAttribute("packages", packages);
-
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return "error";
         }
-
-        return "redirect:/CreatePack"; 
+        return "packageList";
     }
-
-
 
     @PostMapping("/UpdatePack")
     public String updatePack(@ModelAttribute("package") PackageModel packages) {
-        try {
-            Connection connection = dataSource.getConnection();
-            String sql = "UPDATE public.package SET packname = ?, packactivity = ?, packprice = ? WHERE packid = ?";
-            final var statement = connection.prepareStatement(sql);
-    
-            Integer packID = packages.getPackID();
-            String packName = packages.getPackName();
-            String packActivity = packages.getPackActivity();
-            double packPrice = packages.getPackPrice();
-    
-            statement.setString(1, packName);
-            statement.setString(2, packActivity);
-            statement.setDouble(3, packPrice);
-            statement.setInt(4, packID);
-    
+        String sql = "UPDATE public.package SET packname = ?, packactivity = ?, packprice = ? WHERE packid = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, packages.getPackName());
+            statement.setString(2, packages.getPackActivity());
+            statement.setDouble(3, packages.getPackPrice());
+            statement.setInt(4, packages.getPackID());
+
             statement.executeUpdate();
-            connection.close();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return "redirect:/error";
         }
-    
         return "redirect:/CreatePack";
     }
-     
 
     @PostMapping("/DeletePack")
     public String deletePack(@RequestParam("packID") Integer packID) {
-       try {
-        Connection connection = dataSource.getConnection();
         String sql = "DELETE FROM public.package WHERE packid = ?";
-        final var statement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-        statement.setInt(1, packID);
-
-        statement.executeUpdate();
-        connection.close();
-      } catch (Exception e) {
-        e.printStackTrace();
-        return "redirect:/error";
-      }
-
-     return "redirect:/CreatePack";
-  }
-    
+            statement.setInt(1, packID);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "redirect:/error";
+        }
+        return "redirect:/CreatePack";
+    }
 }
