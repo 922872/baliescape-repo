814e20309b9c9a06e01d15bb568937ebf7328b63
922872 <2022675594@student.uitm.edu.my>
2024-06-21 01:59:42 +0800

    a

--- src/main/java/com/heroku/java/controller/PackageController.java
+++ src/main/java/com/heroku/java/controller/PackageController.java
@@ -3,18 +3,10 @@ package com.heroku.java.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.*;
 import com.heroku.java.model.PackageModel;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,7 +35,7 @@ public class PackageController {
             e.printStackTrace();
             return "redirect:/error";
         }
-        return "redirect:/CreatePack";
+        return "redirect:/packageList";
     }
 
     @GetMapping("/packageList")
@@ -72,6 +64,31 @@ public class PackageController {
         return "ViewPack";
     }
 
+    @GetMapping("/updatePackages")
+    public String showUpdateForm(@RequestParam("packID") Integer packID, Model model) {
+        PackageModel packageModel = new PackageModel();
+        String sql = "SELECT packid, packname, packactivity, packprice FROM public.package WHERE packid = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, packID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                packageModel.setPackID(resultSet.getInt("packid"));
+                packageModel.setPackName(resultSet.getString("packname"));
+                packageModel.setPackActivity(resultSet.getString("packactivity"));
+                packageModel.setPackPrice(resultSet.getDouble("packprice"));
+            }
+
+            model.addAttribute("package", packageModel);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "error";
+        }
+        return "UpdatePack";
+    }
+
     @PostMapping("/UpdatePack")
     public String updatePack(@ModelAttribute("package") PackageModel packages) {
         String sql = "UPDATE public.package SET packname = ?, packactivity = ?, packprice = ? WHERE packid = ?";
@@ -88,10 +105,10 @@ public class PackageController {
             e.printStackTrace();
             return "redirect:/error";
         }
-        return "redirect:/CreatePack";
+        return "redirect:/packageList";
     }
 
-    @PostMapping("/DeletePack")
+    @GetMapping("/deletePackages")
     public String deletePack(@RequestParam("packID") Integer packID) {
         String sql = "DELETE FROM public.package WHERE packid = ?";
         try (Connection connection = dataSource.getConnection();
@@ -103,6 +120,6 @@ public class PackageController {
             e.printStackTrace();
             return "redirect:/error";
         }
-        return "redirect:/CreatePack";
+        return "redirect:/packageList";
     }
 }
