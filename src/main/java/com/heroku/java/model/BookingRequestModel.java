77c9600667121836ac5ac94ba7dea9a4262273fe
rafiqahizwan <2022864926@student.uitm.edu.my>
2024-07-01 21:03:40 +0800

    BookingModel.java

--- src/main/java/com/heroku/java/model/BookingRequestModel.java
+++ src/main/java/com/heroku/java/model/BookingRequestModel.java
@@ -1,73 +1,59 @@
 package com.heroku.java.model;
 
-import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class BookingRequestModel {
-    private Integer requestId;
-    private String customerName;
-    private String email;
-    private Date travelDate;
-    private String packageType;
-    private int noOfPax;
-    private String status;
+@Entity
+public class Booking {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer bookingID;
+  private String package;
+  private String packageType;
+  private int noOfPack;
+  private String duration;
 
-    public BookingRequestModel() {
-        // Default constructor
-    }
+  // Getters and Setters
+  public Integer getBookingID() {
+    return bookingID;
+  }
 
-    public Integer getRequestId() {
-        return requestId;
-    }
+  public void setBookingID(Integer bookingID) {
+    this.bookingID = bookingID;
+  }
 
-    public void setRequestId(Integer requestId) {
-        this.requestId = requestId;
-    }
+  public String getPackage() {
+    return package;
+  }
 
-    public String getCustomerName() {
-        return customerName;
-    }
+  public void setPackage(String package) {
+    this.package = package;
+  }
 
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
+  public String getPackageType() {
+    return packageType;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setPackageType(String packageType) {
+    this.packageType = packageType;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public int getNoOfPack() {
+    return noOfPack;
+  }
 
-    public Date getTravelDate() {
-        return travelDate;
-    }
+  public void setNoOfPack(int noOfPack) {
+    this.noOfPack = noOfPack;
+  }
 
-    public void setTravelDate(Date travelDate) {
-        this.travelDate = travelDate;
-    }
+  public String getDuration() {
+    return duration;
+  }
 
-    public String getPackageType() {
-        return packageType;
-    }
-
-    public void setPackageType(String packageType) {
-        this.packageType = packageType;
-    }
-
-    public int getNoOfPax() {
-        return noOfPax;
-    }
-
-    public void setNoOfPax(int noOfPax) {
-        this.noOfPax = noOfPax;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+  public void setDuration(String duration) {
+    this.duration = duration;
+  }
 }
